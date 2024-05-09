eb6805243857e6fa2254bc5557360ffa48ec0eab
Max Slobodyanuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2024-05-09 16:14:55 +0300

    add component Notification

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react";
 import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
+import Notification from "../Notification/Notification";
 import css from "./App.module.css";
 
 const getFeedback = () => {
-    const saveFeedback = localStorage.getItem("feedbacks");
-    if (saveFeedback !== null) {
-        return JSON.parse(saveFeedback);
-    }
-    return {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-}
+  const saveFeedback = localStorage.getItem("feedbacks");
+  if (saveFeedback !== null) {
+    return JSON.parse(saveFeedback);
+  }
+  return {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+};
 
 const App = () => {
   const [feedback, setFeedback] = useState(getFeedback);
@@ -45,13 +46,13 @@ const App = () => {
       bad: 0,
     });
   };
-    
-  useEffect(() => {
-      localStorage.setItem("feedbacks", JSON.stringify(feedback));
-  }, [feedback])
 
-    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-    const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+  useEffect(() => {
+    localStorage.setItem("feedbacks", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
 
   return (
     <div className={css.container}>
@@ -68,7 +69,7 @@ const App = () => {
           positiveFeedback={positiveFeedback}
         />
       ) : (
-        "No feedback yet"
+        <Notification />
       )}
     </div>
   );
